68d964b2f5eb02e2cda5fd950377bfbe3531bcea
filip-dot-cube <filip.kubik@ackee.cz>
2020-02-19 19:25:18 +0100

    :recycle: Move locale data to site as example, keep polyfill in thme
    Move locale data to site as example, keep polyfill in thme

--- gatsby-theme-localized/src/i18n/config/reactIntl.js
+++ gatsby-theme-localized/src/i18n/config/reactIntl.js
@@ -1,7 +1,3 @@
-// react-intl no longer supports addLocaleData for plurals, you can polyfill it with @formatjs like this
-
 if (!Intl.PluralRules) {
     require('@formatjs/intl-pluralrules/polyfill');
-    require('@formatjs/intl-pluralrules/dist/locale-data/en'); // Add locale data for de
-    require('@formatjs/intl-pluralrules/dist/locale-data/cs'); // Add locale data for cs
 }
