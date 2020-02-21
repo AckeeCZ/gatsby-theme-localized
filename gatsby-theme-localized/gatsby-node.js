d54405e26ecd29ca4f4535d0c5364e81183d7785
filip-dot-cube <filip.kubik@ackee.cz>
2020-02-21 17:50:46 +0100

    :recycle: Revert canvas polyfill
    Revert canvas polyfill

--- gatsby-theme-localized/gatsby-node.js
+++ gatsby-theme-localized/gatsby-node.js
@@ -34,4 +34,17 @@ exports.onCreatePage = ({ page, actions }, themeOptions) => {
     });
 };
 
-
+exports.onCreateWebpackConfig = ({ stage, rules, loaders, plugins, actions }) => {
+    if (stage === 'build-html' || stage === 'develop-html') {
+        actions.setWebpackConfig({
+            module: {
+                rules: [
+                    {
+                        test: /canvas/,
+                        use: loaders.null(),
+                    },
+                ],
+            },
+        });
+    }
+};
